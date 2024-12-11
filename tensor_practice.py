cabe2e50ffe845e69441dc9906d83c136f654458
Amadeus Kaczmarek <amadeuskaczmarek@gmail.com>
2024-12-11 01:02:46 -0700

    December 11th 2024

--- tensor_practice.py
+++ tensor_practice.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
 
 tensor1 = tf.Variable([["Test","OK"],["Hehe","AHHH"]])
-print(tensor1.shape)
+#print(tensor1.shape)
+
+t = tf.zeros([2,1,2]) #2 lists that contains 1 lists that contains 2 0's 
+print(t)
+
+t = tf.reshape(t, [4,1])
+print(t)
